7d0e5e3f249aa325313ff171aed6b0657ea1142a
Tharushika Dilmini <dilz2001.1221@gmail.com>
2024-10-04 14:57:41 +0530

    done some changes

--- src/pages/EleganceAI/EleganceAI.jsx
+++ src/pages/EleganceAI/EleganceAI.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function EleganceAI() {
   return (
-    <div>EleganceAI</div>
+    <div className="container-fluid p-0">
+    <img
+      src='ai.png'
+      alt="carsoulimg"
+      className="service img-fluid w-100"
+    />
+
+  </div>
+
   )
 }
